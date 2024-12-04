d7d3bb5f77cba9a7b22554dee665b2335a23c69d
JesusContreras26 <jgcc26@gmail.com>
2024-12-04 12:15:01 -0400

    drugs controllers and validation finish

---
+++ middleware/validatorDrugs.js
@@ -0,0 +1,54 @@
+const { body, validationResult } = require('express-validator');
+const validate = {};
+
+validate.drugsRules = () => {
+  return [
+    body('commercialName')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isLength({ min: 1 })
+      .withMessage('Please provide a valid Commercial Name'),
+    body('scientificName')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isLength({ min: 1 })
+      .withMessage('Please provide a valid Scientific Name'),
+    body('VendorId')
+      .notEmpty()
+      .exists({ checkFalsy: true })
+      .isLength({ min: 24 })
+      .withMessage('Please provide a valid VendorId'),
+    body('expireDate')
+      .notEmpty()
+      .trim()
+      .isLength({ min: 1 })
+      .isISO8601()
+      .withMessage('Please provide a valid date'),
+    body('doses')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 1 })
+      .notEmpty()
+      .withMessage('Please provide a valid dose'),
+    body('description')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isLength({ min: 10 })
+      .withMessage('Please provide a valid description'),
+  ];
+};
+
+validate.checkDrugsData = (req, res, next) => {
+  let errors = [];
+  errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array().map((element) => {
+        return element.msg;
+      }),
+    });
+  }
+  next();
+};
+
+module.exports = validate;
