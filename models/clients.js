9761134fbe5e3b7104557b7d2222175cc8677ca3
erickandreww <erick.andrew.moura@gmail.com>
2024-12-14 12:51:29 -0300

    updating clients model to work with the store collection

--- models/clients.js
+++ models/clients.js
@@ -6,7 +6,11 @@ const clientSchema = new schema({
     client_phone: {type: String, required: true},
     client_address: {type: String},
     client_ssid: {type: String, unique: true, immutable: true},
-    client_purchases: []
+    client_purchases: [{
+        purchase_date: {type: String, required: true},
+        drug_id: {type: schema.Types.ObjectId, ref: "Drug"}, 
+        quantity: {type: String, required: true},
+    }]
 },
 {
     timestamps: true
