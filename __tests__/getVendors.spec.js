c2b70eccbfd510618e993a2380c85fb586a1f4cc
heitor <heitorcazado@gmail.com>
2024-12-14 16:28:27 -0300

    added vendor object to compare response body

--- __tests__/getVendors.spec.js
+++ __tests__/getVendors.spec.js
@@ -1,6 +1,15 @@
 const app = require('../server');
 const request = require('supertest');
 const SECONDS = 10000;
+const vendor = {
+  _id: "675067f271e5462284991729",
+  name: "Bayer",
+  phone: "862-404-3000",
+  address: "100 Bayer Boulevard Whippany, NJ 07981",
+  rating: 5,
+  itin: "123-45-6789",
+};
+
 describe('Testing vendors routes', () => {
   test(
     'Getting all vendors',
@@ -16,9 +25,10 @@ describe('Testing vendors routes', () => {
     'Finding a vendor by name',
     async () => {
       const res = await request(app)
-        .get('/vendors/findbyname/bayer')
+        .get(`/vendors/findbyname/${vendor.name}`)
         .expect('Content-Type', /json/)
         .expect(200);
+        expect(JSON.parse(res.text)).toMatchObject(vendor)
     },
     50 * SECONDS
   );
@@ -26,9 +36,10 @@ describe('Testing vendors routes', () => {
     'Finding a vendor by itin',
     async () => {
       const res = await request(app)
-        .get('/vendors/findbyitin/123-54-6897')
+        .get(`/vendors/findbyitin/${vendor.itin}`)
         .expect('Content-Type', /json/)
         .expect(200);
+        expect(JSON.parse(res.text)).toMatchObject(vendor);
     },
     50 * SECONDS
   );
