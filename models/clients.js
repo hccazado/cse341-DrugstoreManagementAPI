38b8abb27b16ab244f9a20b4d672b0752b8b7bcc
erickandreww <erick.andrew.moura@gmail.com>
2024-12-17 20:13:04 -0300

    Removing required

--- models/clients.js
+++ models/clients.js
@@ -9,9 +9,9 @@ const clientSchema = new schema(
     client_ssid: { type: String, unique: true, immutable: true },
     client_purchases: [
       {
-        purchase_date: { type: String, required: true },
-        drug_id: { type: schema.Types.ObjectId, ref: 'Drug' },
-        quantity: { type: String, required: true },
+        purchase_date: { type: String},
+        drug_id: { type: schema.Types.ObjectId, ref: 'Drug'},
+        quantity: { type: String},
       },
     ],
   },
